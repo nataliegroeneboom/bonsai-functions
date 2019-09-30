00c9031bba1c1930d800a5ba7027d3f143fdd1da
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-09-30 16:32:56 +0100

    add express and changed function to use api prefix

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,38 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
 
 var serviceAccount = require("./serviceAccountKey.json");
+
+const app = express();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://bonsai-7041d.firebaseio.com"
 });
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.getBonsais = functions.https.onRequest((req, res) => {
- admin.firestore().collection('bonsais').get()
+app.get('/bonsais', (req, res) => {
+    admin.firestore()
+    .collection('bonsais')
+    .orderBy('name')
+    .get()
     .then(data => {
         let bonsais = [];
         data.forEach(doc => {
-            bonsais.push(doc.data())
+            bonsais.push({
+                bonsaiId: doc.id,
+                body: doc.data().body,
+                name: doc.data().name,
+                family: doc.data().family
+            })
         });
         return res.json(bonsais);
     })
     .catch(err => console.error(err));
-});
+})
 
-exports.createBonsai = functions.https.onRequest((req, res) => {
+app.post('/bonsai', (req, res) => {
     const newBonsai = {
         body: req.body.body,
         family: req.body.family,
@@ -45,3 +50,4 @@ exports.createBonsai = functions.https.onRequest((req, res) => {
         })
    });
    
+exports.api = functions.region('europe-west1').https.onRequest(app);
