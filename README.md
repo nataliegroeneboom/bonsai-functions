0f2fc43f82f0f61cccf5590e09b72cb128f69cdd
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-10-01 11:07:55 +0100

    add user authentication

--- README.md
+++ README.md
@@ -3,15 +3,16 @@
 ## Part One: Firebase
 - [x] display trees
 - [x] ability to add tree
+- [ ] add user signup
+- [ ] add user login
 - [ ] image handling
 - [ ] edit image
 - [ ] edit content
 - [ ] delete content
 - [ ] display individual tree
 - [ ] filter on trees
-- [ ] add user
-- [ ] add user authentication
 - [ ] add ability to comment
+- [ ] add validation
 - [ ] add error handling
 
 
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,37 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
+const app = require('express')();
 
-var serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
-const app = express();
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrs2uuJWyn3jC3ROb3gVh9pHeds3OCLJU",
+    authDomain: "bonsai-7041d.firebaseapp.com",
+    databaseURL: "https://bonsai-7041d.firebaseio.com",
+    projectId: "bonsai-7041d",
+    storageBucket: "bonsai-7041d.appspot.com",
+    messagingSenderId: "13325179480",
+    appId: "1:13325179480:web:197746bd90938831ff882d",
+    measurementId: "G-8P23B54DLT"
+  };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://bonsai-7041d.firebaseio.com"
 });
 
+const firebaseLib = require('firebase');
+firebaseLib.initializeApp(firebaseConfig);
 
+const db = admin.firestore();
+
+//==================================================================================================================================
+//=========================================================GET BONSAIS==============================================================
+//==================================================================================================================================
 app.get('/bonsais', (req, res) => {
-    admin.firestore()
+    db
     .collection('bonsais')
     .orderBy('name')
     .get()
@@ -31,14 +49,16 @@ app.get('/bonsais', (req, res) => {
     })
     .catch(err => console.error(err));
 })
-
+//==================================================================================================================================
+//=========================================================POST BONSAI==============================================================
+//==================================================================================================================================
 app.post('/bonsai', (req, res) => {
     const newBonsai = {
         body: req.body.body,
         family: req.body.family,
         name: req.body.name
     };
-    admin.firestore()
+    db
         .collection('bonsais')
         .add(newBonsai)
         .then(doc => {
@@ -49,5 +69,63 @@ app.post('/bonsai', (req, res) => {
             res.status(500).json({error: 'something when wrong'})
         })
    });
+
+//==================================================================================================================================
+//=========================================================SIGN UP==================================================================
+//==================================================================================================================================
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+        admin: false
+    }
+    let token, userId; 
+    db.doc(`/users/${newUser.handle}`)
+        .get()
+        .then(doc => {
+             if(doc.exists){
+                 return res.status(400).json({handle: 'this username is already taken'});
+             }else{
+                return firebaseLib.auth()
+                .createUserWithEmailAndPassword(newUser.email, newUser.password)
+             }
+        })
+        .then(data => {
+            // we have our user created, we can get the token
+            userId = data.user.uid;
+           return data.user.getIdToken()
+
+        })
+        .then(tokenId => {
+            token = tokenId;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                admin: newUser.admin,
+                userId
+            };
+           return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+           
+           
+        })
+        .then(() => {
+            return res.status(201).json({token}); 
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({email: 'Email already in use'})    
+            }else{
+                return res.status(500).json({error: err.code})
+            }
+              
+        });
+
+});
+
    
 exports.api = functions.region('europe-west1').https.onRequest(app);
